a6e6cb737014326398c127bc7ec67686ecc0f82d
John Chen <john.data.chen@facebook.com>
2015-02-09 23:07:12 +0800

    TWD-USD + TWSE_stocks ETL updated
    1. 匯率新增註解說明
    2. 股票價格資料第一版完成：日期 成交股數...這表頭每個月前頭都會出現一次，

--- TWD-USD.py
+++ TWD-USD.py
@@ -7,36 +7,39 @@ from bs4 import BeautifulSoup
 # 要是沒有這個資料夾，新建一個
 if not os.path.exists("TWD-USD"):
     os.mkdir("TWD-USD")
-result =open("TWD-USD/TWD-USD.csv",'w') # 寫入模式：開頭用w，後來改為a，才能將資料都寫入清單
+result =open("TWD-USD/TWD-USD.txt",'w') # 寫入模式：2006年用w，(含)2007後改為a，才能將資料都寫入清單
 
 # 請在這裡修改日期，由於上限是"一年"，請改成XXXX/1/1 ~ XXXX/12/31
+# 最早只到2006/5/29, 2008/6/23以前只有美金匯率
 payload = {
     "download":"",
     "hdn_gostartdate":"2015/1/1",
-    "hdn_goenddate":"2015/2/9",
+    "hdn_goenddate":"2015/12/31",
     "syear":"2015",
     "smonth":"1",
     "sday":"1",
     "eyear":"2015",
-    "emonth":"2",
-    "eday":"9",
+    "emonth":"12",
+    "eday":"31",
     "datestart":"2015/1/1",
-    "dateend":"2015/2/9"
+    "dateend":"2015/12/31"
 }
 res = requests.post("http://www.taifex.com.tw/chinese/3/3_5.asp", data=payload)
-#print res.encoding # 網頁編碼
+#print res.encoding # 找出網頁編碼
 soup = BeautifulSoup(res.text.encode("ISO-8859-1"))
 
 counter = 0
-for item in soup.select(".table_c tr"):
+for item in soup.select(".table_c tr")[1:]: # 除了2006以外，(含)2007年起必須加[1:]去掉表頭
     counter += 1
     if counter == 1:
         t = item.text.split("\n")
         for item in t:
-            result.write(item.encode('utf-8')  + " "),
+            result.write(item.encode('utf-8') + " "),
             print item,
         counter = 0
         result.write(item.encode('utf-8') + "\n")
         print ""
+
+# 提示結束跟關閉寫檔
 print "清單產生完畢"
 result.close()
--- TWSE_stocks.py
+++ TWSE_stocks.py
@@ -1,23 +1,47 @@
 #! coding=UTF-8
-import requests
+__author__ = 'john.chen'
+
+import requests, time, os
 from bs4 import BeautifulSoup
 
-group = ['3607']
+# 要是沒有這個資料夾，新建一個
+if not os.path.exists("TWSE_Stocks"):
+    os.mkdir("TWSE_Stocks")
 
-for i in range(2014,2013,-1): # 2014年 = 民國103年
-    for j in range(1,13): # 1~12月
-        year = i
-        if j < 10: # 如果是1~9月前面加0，01~09
-            month = "0" + str(j)
-        else:
-            month = j # 10以上就原樣輸出
-        for stock in group:   # 跑股票代碼
-            bid_detail=open(format(stock) + "_bid_detail.txt",'a') # 改成format(stock) + 檔名.txt，模式改為'a' = 附加模式
-            url = "http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY_AVG/genpage/Report{}{}/{}{}_F3_1_8_{}.php?STK_NO={}&myear={}&mmon={}#"
-            res = requests.get(url.format(year,month,year,month,stock,stock,year,month))     # {}放參數的意思，year：年，month：月，stock：股票代號
-            # print res.encoding # 網頁編碼
+# 股票代碼表
+group = ['0050','4938','3481','2330','2303','2882','2357','1303','2883',\
+         '1301','2002','2311','2317','1402','2892','2880','2801','1216',\
+         '1101','1102','2382','2308','1326','2886','2891','2325','2105',\
+         '2395','2408','2412','2409','2207','2301','9904','2912','2354',\
+         '2474','3045','2454','2881','2887','4904','2885','3008','2498',\
+         '2884','2890','6505','5880','2227','3474']
+
+for stock in group:   # 每個代碼新建一個txt
+    bid_detail=open("TWSE_Stocks/" + format(stock) + "_bid_detail.txt",'w') # 改成format(stock) + 檔名.txt
+    for year in range(2013,2014): # 2014年 - 1911 = 民國103年
+        for a in range(1,13): # 1~12月
+            if a < 10: # 如果是1~9月前面加0，01~09
+                month = "0" + str(a)
+            else:
+                month = a # 10以上就原樣輸出
+
+            url = "http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY/genpage/Report{}{}/{}{}_F3_1_8_{}.php?STK_NO={}&myear={}&mmon={}#"
+            res = requests.get(url.format(year,month,year,month,stock,stock,year,month)) # {}放參數的意思，year：年，month：月，stock：股票代號
+            # print res.encoding # 找出網頁編碼
             soup = BeautifulSoup(res.text.encode('ISO-8859-1'))
-            for td in soup.select(".board_trad tr"):    # .board_trad .是select的用方
-                bid_detail.write(td.text.encode('utf-8')+'\n')
-                print td.text
+
+            counter = 0
+            for i in soup.select(".basic2 td"): # 更改標籤
+                bid_detail.write(i.text.strip().encode('utf-8') + " "), # 每次寫入時以空白隔開, ","代表連續寫不換行
+                print i.text,
+                counter += 1 # 每寫一格，counter + 1
+                if counter == 9: # 當寫到第9格的時候代表該換行了
+                    bid_detail.write("\n"), # 換行
+                    print ""
+                    counter = 0 # 歸零重來
+                    # 日期 成交股數...這表頭每個月前頭都會出現一次，是可以手動刪除，但為了之後方便，應該要改
+
+# 提示結束, 關閉寫檔, delay
+print "清單產生完畢"
 bid_detail.close()
+time.sleep(5)
